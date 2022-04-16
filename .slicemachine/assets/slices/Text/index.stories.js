60b958e4b785792993ceb99b307107785d5b8611
Jung No <jung@jungno.com>
2022-04-15 16:14:53 -1000

    slices

---
+++ .slicemachine/assets/slices/Text/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Text';
+
+export default {
+  title: 'slices/Text'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","name":"Default","slice_type":"text","items":[],"primary":{"text":[{"type":"paragraph","text":"Mollit aute eu nulla tempor duis Lorem ut. Nostrud cillum ipsum ea tempor mollit anim eiusmod fugiat aute deserunt voluptate ex. Nulla sit ipsum amet.","spans":[]}]},"id":"_Default"}} />
+_Default.storyName = 'Default'
